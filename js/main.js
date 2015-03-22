4949e45dbdf6cfef56a6a4c650a7f2c6b8bf9a48
Mathias Peltier <mpeltier@eurodns.com>
2015-03-22 16:50:52 +0100

    Use of map and props

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
+var comicList = [{title: "Amazing Spider-Man (2014) #1 (Mhan Variant)"}, {title: "Superior Spider-Man (2013) #22"}];
+
 var ComicApp = React.createClass({
     render: function () {
         return (
             <div>
                 <h1>Comic Viewer</h1>
+                <ul>
+                    {comicList.map(function (comic) {
+                        return (<ComicElement title={comic.title} />)
+                    })}
+                </ul>
             </div>
         );
+    }
+});
+
+var ComicElement = React.createClass({
+    render: function () {
+        return (<li>{this.props.title}</li>);
     }
 });
 

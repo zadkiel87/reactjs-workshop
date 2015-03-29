4e47a52f8f0e9e323f9d674ba1522e46ceaccd61
Mathias Peltier <mpeltier@eurodns.com>
2015-03-29 16:16:59 +0200

    Integration with external lib

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-var comicList = [{title: "Amazing Spider-Man (2014) #1 (Mhan Variant)"}, {title: "Superior Spider-Man (2013) #22"}];
+var comicData = [
+    {
+        title: "Amazing Spider-Man: Renew Your Vows (2015) #1",
+        thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/2/60/5515b41ff2000.jpg"
+    }, {
+        title: "Amazing Spider-Man (2014) #18.1",
+        thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/b/a0/5506e04ad4bad.jpg"
+    }, {
+        title: "Iron Man Infinite Digital Comic (2013) #12",
+        thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/1/03/52b4886020b90.jpg"
+    }, {
+        title: "Daredevil: End of Days (2012) #1",
+        thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/f/70/4ff5b45a7da8a.jpg"
+    }
+];
 
 var ComicApp = React.createClass({
 
     getInitialState: function () {
-        return {comicList: comicList};
+        return {comicList: []};
     },
 
-    _addComic: function (comic) {
+    _addComic: function (comicTitle) {
+        var comic = comicData.find(function (comic) {
+            return comic.title == comicTitle;
+        });
         this.setState({comicList: this.state.comicList.concat(comic)});
     },
 
@@ -17,7 +34,7 @@ var ComicApp = React.createClass({
                 <ComicAdder onSubmit={this._addComic}/>
                 <ul>
                     {this.state.comicList.map(function (comic) {
-                        return (<ComicElement title={comic.title}/>)
+                        return (<ComicElement comic={comic}/>)
                     })}
                 </ul>
             </div>
@@ -29,16 +46,29 @@ var ComicApp = React.createClass({
 var ComicElement = React.createClass({
 
     render: function () {
-        return (<li>{this.props.title}</li>);
+        return (
+            <img
+                src={this.props.comic.thumbnail}
+                title={this.props.comic.title}/>
+        );
     }
 
 });
 
 var ComicAdder = React.createClass({
 
+    componentDidMount: function () {
+        var input = this.refs.inputTitle.getDOMNode();
+        new Awesomplete(input, {
+            list: comicData.map(function (comic) {
+                return comic.title
+            })
+        });
+    },
+
     _onSubmit: function (e) {
         e.preventDefault();
-        this.props.onSubmit({title: this.refs.inputTitle.getDOMNode().value})
+        this.props.onSubmit(this.refs.inputTitle.getDOMNode().value)
     },
 
     render: function () {
